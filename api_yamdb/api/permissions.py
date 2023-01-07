f9e4146d1f95e06286de369648a64bf3e511f561
Krugger1982 <razumyanchik@mail.ru>
2023-01-07 23:34:39 +0300

    updated permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,14 +3,9 @@ from rest_framework import permissions
 
 class IsModeratorOrAuthorOrReadOnly(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
         return (
-            request.user.role == 'Moderator'
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'Moderator'
             or obj.owner == request.user
         )
